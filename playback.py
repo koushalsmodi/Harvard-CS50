ca50aa277c689c4f2f74aadce541e3b876eb5a4c
koushalsmodi <124229657+koushalsmodi@users.noreply.github.com>
2023-08-21 18:36:40 -0700

    Add files via upload

---
+++ playback.py
@@ -0,0 +1,2 @@
+user = input('Enter speech: ').replace(' ', '...') # using replace function to replace space between the words with 3 periods
+print(user) # printing the user input after applying the string method

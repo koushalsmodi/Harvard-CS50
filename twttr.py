3cd3b23cd59ba883df3804956e42adca828ed8ed
koushalsmodi <124229657+koushalsmodi@users.noreply.github.com>
2023-08-21 18:09:42 -0700

    Add files via upload

---
+++ twttr.py
@@ -0,0 +1,14 @@
+def main():
+    text = input('Input: ')
+    output = shorten(text)
+    print(f"Output: {output}")
+
+
+def shorten(word):
+    vowels = ["a", "e", "i", "o", "u"]
+    shortened = []
+    for i in word:
+        if i.casefold() not in vowels:
+            shortened.append(i)
+    output = str("".join(shortened))
+    return output

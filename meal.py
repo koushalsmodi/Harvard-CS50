91354abfbf2fb0cff8d13155e32bec0ce80a1b49
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-11-23 21:12:59 -0800

    Update meal.py

--- meal.py
+++ meal.py
@@ -3,10 +3,13 @@ def main():
     time = input('What time is it? ')
     # converting time here
     c_time = convert(time)
+    # between 7 and 8 am inclusive, breakfast time
     if 7 <= c_time <= 8:
         print('breakfast time')
+    # between 12 and 1 pm inclusive, lunch time
     elif 12 <= c_time <= 13:
         print('lunch time')
+    # between 6 and 7 pm inclusive, dinner time
     elif 18 <= c_time <= 19:
         print('dinner time')
 
